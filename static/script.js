4f6801b8c1e0989953aecfb0bf081e75335d684b
ngfritz <nemethng@gmail.com>
2024-06-05 18:21:50 +0200

    Add page display ok

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+// things for the uername feature
 const welcome = document.getElementById ("welcome");
 const enterUsername = document.getElementById("enterUsername");
 const navUsername = document.getElementById("navUsername");
@@ -27,5 +28,29 @@ else {
 }
 
 
+// things for the added drug display
 
+const drug_name = document.getElementById ("drug_name");
+const effect_type = document.getElementById ("effect_type");
+const exp_date = document.getElementById ("exp_date"); 
+const active_ingredient = document.getElementById ("active_ingredient");
+const storage_location = document.getElementById ("storage_location");
+const stock = document.getElementById ("stock");
+const other = document.getElementById ("other");
+const added_drug = document.getElementById ("added_drug");
+const addNewDrugButton = document.getElementById("addNewDrugButton");
+
+
+
+function saveDrug(){
+    localStorage.setItem("Drug Name", drug_name.value);
+    localStorage.setItem("Field of Effect", effect_type.value);
+    localStorage.setItem("Expiry Date", exp_date.value);
+    localStorage.setItem("Active Ingredient", active_ingredient.value);
+    localStorage.setItem("Storage Location", storage_location.value);
+    localStorage.setItem("Stock available", stock.value);
+    localStorage.setItem("Other Comment", other.value);
+}
+
+addNewDrugButton.addEventListener("click",saveDrug);
 
--- static/script_add.js
+++ static/script_add.js
@@ -1,3 +1,4 @@
+// things for the username
 const navUsername = document.getElementById("navUsername");
 const user = localStorage.getItem("username");
 
@@ -7,41 +8,44 @@ if ( user != null){
     navUsername.innerHTML = "Username: " + user;
 }
 
-// const drug_name = document.getElementById ("drug_name");
-// const effect_type = document.getElementById ("effect_type");
-// const exp_date = document.getElementById ("exp_date"); 
-// const active_ingredient = document.getElementById ("active_ingredient");
-// const storage_location = document.getElementById ("storage_location");
-// const stock = document.getElementById ("stock");
-// const other = document.getElementById ("other");
-// const added_drug = document.getElementById ("added_drug");
-// const addNewDrugButton = document.getElementById("addNewDrugButton");
-// const backToAddButton = document.getElementById("backToAddButton");
+
+// things for the added drug display
+
+const drug_name = document.getElementById ("drug_name");
+const effect_type = document.getElementById ("effect_type");
+const exp_date = document.getElementById ("exp_date"); 
+const active_ingredient = document.getElementById ("active_ingredient");
+const storage_location = document.getElementById ("storage_location");
+const stock = document.getElementById ("stock");
+const other = document.getElementById ("other");
+const added_drug = document.getElementById ("added_drug");
+const addNewDrugButton = document.getElementById("addNewDrugButton");
+const backToAddButton = document.getElementById("backToAddButton");
 
 
-// function saveDrug(){
-//     localStorage.setItem("Drug Name", drug_name.value);
-//     localStorage.setItem("Field of Effect", effect_type.value);
-//     localStorage.setItem("Expiry Date", exp_date.value);
-//     localStorage.setItem("Active Ingredient", active_ingredient.value);
-//     localStorage.setItem("Storage Location", storage_location.value);
-//     localStorage.setItem("Stock available", stock.value);
-//     localStorage.setItem("Other Comment", other.value);
-//     dn = localStorage.getItem("Drug Name");
-//     et = localStorage.getItem("Field of Effect");
-//     ed = localStorage.getItem("Expiry Date");
-//     ai = localStorage.getItem("Active Ingredient");
-//     sl = localStorage.getItem("Storage Location");
-//     stk = localStorage.getItem("Stock available");
-//     oth = localStorage.getItem("Other Comment");
-//     table = "<p>Drug Name: " + dn + "</p><p>Fiels of Effect: " + et + "</p><p>Expiry Date: " + ed + "</p><p>Active Ingredient: " + ai + "</p><p>Storage Location: " + sl + "</p><p>Stock available: " + stk + "</p><p>Other comments: " + oth + "</p>"; 
-//     added_drug.innerHTML = table;
-// };
+function savedDrug(){
+    dn = localStorage.getItem("Drug Name");
+    et = localStorage.getItem("Field of Effect");
+    ed = localStorage.getItem("Expiry Date");
+    ai = localStorage.getItem("Active Ingredient");
+    sl = localStorage.getItem("Storage Location");
+    stk = localStorage.getItem("Stock available");
+    oth = localStorage.getItem("Other Comment");
+    table = "<p>Drug Name: <b>" + dn + "</b></p><p>Fiels of Effect: <b>" + et + "</b></p><p>Expiry Date: <b>" + ed + "</b></p><p>Active Ingredient: <b>" + ai + "</b></p><p>Storage Location: <b>" + sl + "</b></p><p>Stock available: <b>" + stk + "</b></p><p>Other comments: <b>" + oth + "</b></p>"; 
+    added_drug.innerHTML = table;
+};
 
-// function clean(){
-//     localStorage.clear();
-// };
+function clean(){
+    localStorage.removeItem("Drug Name");
+    localStorage.removeItem("Field of Effect");
+    localStorage.removeItem("Expiry Date");
+    localStorage.removeItem("Active Ingredient");
+    localStorage.removeItem("Storage Location");
+    localStorage.removeItem("Stock available");
+    localStorage.removeItem("Other Comment");
+
+};
 
 
-// addNewDrugButton.addEventListener("click",saveDrug);
-// backToAddButton.addEventListener("click",clean);
+savedDrug();
+backToAddButton.addEventListener("click",clean);
